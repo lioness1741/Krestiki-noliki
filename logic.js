7a7ff148c27fbc30009e262222cc38711ca4d1b5
Tatyana <lioness1741@mail.ru>
2023-03-23 23:21:43 +0500

    Доработано

--- logic.js
+++ logic.js
@@ -1,75 +1,70 @@
-let players = ["x", "o"];
+let players = ['x', 'o'];
 let activePlayer = 0;
 let board;
-
-let stepX = [];
-let stepO = [];
 let cellCount = 0;
-
-let winnerPosition = [
-  ["0", "0", "0", "1", "0", "2"],
-  ["1", "0", "1", "1", "1", "2"],
-  ["2", "0", "2", "1", "2", "2"],
-  ["0", "0", "1", "0", "2", "0"],
-  ["0", "1", "1", "1", "2", "1"],
-  ["0", "2", "1", "2", "2", "2"],
-  ["0", "2", "1", "1", "2", "0"],
-  ["0", "0", "1", "1", "2", "2"]
-];
 
 function startGame() {
   board = [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""]
+    ["", "", ""], 
+    ["", "", ""], 
+    ["", "", ""], 
   ];
-  activePlayer = players[Math.floor(Math.random() * 2)];
-  console.log("Первым начинает игрок: " + activePlayer);
 
+  activePlayer = players[Math.floor(Math.random() * 2)];
+  alert("Первым начинает игрок: " + activePlayer);
+  
   renderBoard(board);
   cellCount = 0;
 }
 
-function click(numberRow, numberCol) {
-  board[numberRow][numberCol] = activePlayer;
-
+function click(numRow, numCol) {
+  
+  board[numRow][numCol] = activePlayer;
   renderBoard(board);
   cellCount++;
-  if (cellCount === 9) {
+   if (activePlayer === players[0]) {
+      activePlayer = players[1];    
+    } else if ((activePlayer = players[1])) {
+      activePlayer = players[0];   
+    }
+
+   if (
+    (board[0][0] === activePlayer &&
+      board[0][1] === activePlayer &&
+      board[0][2] === activePlayer) ||
+    
+    (board[1][0] === activePlayer &&
+      board[1][1] === activePlayer &&
+      board[1][2] === activePlayer) ||
+    
+    (board[2][0] === activePlayer &&
+      board[2][1] === activePlayer &&
+      board[2][2] === activePlayer) ||
+    
+    (board[0][0] === activePlayer &&
+      board[1][0] === activePlayer &&
+      board[2][0] === activePlayer) ||
+    
+    (board[0][1] === activePlayer &&
+      board[1][1] === activePlayer &&
+      board[2][1] === activePlayer) ||
+    
+    (board[0][2] === activePlayer &&
+      board[1][2] === activePlayer &&
+      board[2][2] === activePlayer) ||
+    
+    (board[0][0] === activePlayer &&
+      board[1][1] === activePlayer &&
+      board[2][2] === activePlayer) ||
+    
+    (board[0][2] === activePlayer &&
+      board[1][1] === activePlayer &&
+      board[2][0] === activePlayer)
+  ) {
+     showWinner(alert("Выиграл игрок " + activePlayer);
+}
+  else if(cellCount === 9) {
     alert("Ничья");
   }
-  if (activePlayer === players[0]) {
-    stepX.push(numberRow);
-    stepX.push(numberCol);
-  } else if (activePlayer === players[1]) {
-    stepO.push(numberRow);
-    stepO.push(numberCol);
-  }
-  console.log(stepX);
-  console.log(stepO);
 }
 
-function showWinner() {
-  let showWinList;
-  for (let i = 0; i < winnerPosition.length; i++) {
-    let winnerArr = winnerPosition[i];
-    let position = [];
-    for (let j = 0; j < winnerArr.length; j++) {
-      if (j % 2 == 0) {
-        let firstW = winnerArr[j];
-        secondW = winnerArr[j - 1];
-        position.push(board[firstW][secondW]);
-      }
-    }
-    if (position[0] == position[1] && position[1] == position[2] && position[2] !== "") {
-
-      if (position[0] == players[0] && position[1] == players[0] && position[2] == players[0]) {
-        showWinList = 'выиграл игрок ' + players[0];
-      } else {
-        showWinList = 'выиграл игрок ' + players[1];
-      }
-    }
-    return showWinList;
-  }
-  return null;
-}
